5a86ce8b196f740281d6b28a77ee6a31abd91b51
itsasoalt <itsaso.altuna@gmail.com>
2016-02-23 18:17:36 +0100

    Updated Master content

---
+++ contactthanks.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Obsocial</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/fixes.css">
+</head>
+<body>
+	
+	<header class="color-one cl">
+			<a href="index.html" class="logo">
+				<img src="img/logo-obsocial.png"  class="logo-obsocial" alt="Logotipo Obsocial">
+			</a>	<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Obsocial</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/fixes.css">
+</head>
+<body>
+	
+	<header class="color-one cl">
+			<a href="index.html" class="logo">
+				<img src="img/logo-obsocial.png"  class="logo-obsocial" alt="Logotipo Obsocial">
+			</a>	
+			<nav>
+				<a class="menu-item" href="eventos.html">Eventos</a>
+				<a class="menu-item" href="index.html#infografia">¿Qué es la Obsolescencia?</a>
+				<a class="menu-item" href="contacto.html">Contacto</a>
+			</nav>
+	</header>
+	<article>
+		<section class="portada portada-contacto">
+			<h1>Contacto</h1>
+			<p>Encuentranos y ponte en contacto con nosotros.</p>
+		</section>
+		<section class="color-three content-space bottom-space">
+		<h3>Gracias por contactarnos, tu mensaje ha sido enviado y pronto tendrás una respuesta</h3>
+		</section>
+	</article>
+	<footer>
+		<img class="logo-obsocial footer" src="img/logo-alt-obso.png" alt="logo obsocial">
+		<div class="footer">
+			<p>Conoce al equipo <a class="link-alt" href="nosotros.html">detras de Obsocial</a></p>
+			<a class="link-footer" href="https://twitter.com/"><img class="img-footer" src="img/logo-twitter.png"></a>
+			<a class="link-footer" href="https://www.facebook.com/"><img class="img-footer" src="img/logo-fb.png"></a>
+		</div>
+	</footer>
+</body>
+</html>
+			<nav>
+				<a class="menu-item" href="eventos.html">Eventos</a>
+				<a class="menu-item" href="index.html#infografia">¿Qué es la Obsolescencia?</a>
+				<a class="menu-item" href="contacto.html">Contacto</a>
+			</nav>
+	</header>
+	<article>
+		<section class="portada portada-contacto">
+			<h1>Contacto</h1>
+			<p>Encuentranos y ponte en contacto con nosotros.</p>
+		</section>
+		<section class="color-three content-space bottom-space">
+			<form method="post" action="contactengine.php">
+				<label for="nombre">Nombre</label>
+				<input type="text" placeholder="Tu nombre">
+				<label for="email">Dirección de correo</label>
+				<input type="text" placeholder="Tu email">
+				<label for="message">Mensaje</label>
+				<textarea placeholder="Tu mensaje"></textarea>
+			</form>
+			<a class="btn" href="#">Enviar mensaje</a>
+		</section>
+	</article>
+	<footer>
+		<img class="logo-obsocial footer" src="img/logo-alt-obso.png" alt="logo obsocial">
+		<div class="footer">
+			<p>Conoce al equipo <a class="link-alt" href="nosotros.html">detrás de Obsocial</a></p>
+			<a class="link-footer" href="https://twitter.com/"><img class="img-footer" src="img/logo-twitter.png"></a>
+			<a class="link-footer" href="https://www.facebook.com/"><img class="img-footer" src="img/logo-fb.png"></a>
+		</div>
+	</footer>
+</body>
+</html>

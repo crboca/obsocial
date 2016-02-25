95a2f511b0c2e3ec3174344f9693ba689a733d0f
Jorge Valencia <info@jorgevalencia.co>
2016-02-25 16:20:01 +0100

    Fix de media querys con viewport.

--- contactthanks.php
+++ contactthanks.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Obsocial</title>
 	<link rel="stylesheet" href="normalize.css">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
@@ -17,6 +18,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Obsocial</title>
 	<link rel="stylesheet" href="normalize.css">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>

3e98868b140505dd78a55c608c8827240f2dfb76
itsaso <itsaso@iMac-de-Itsaso.local>
2016-03-28 20:44:47 +0200

    obsocial web antigua sass

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+        $( ".btn-anim-three" ).on( "click", function() {
+          $(this).toggleClass( "closed" );
+          $('.nav-mobile').toggleClass( "closed" );
+        });
+
+        
+  		});

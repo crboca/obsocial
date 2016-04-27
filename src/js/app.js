e69b55073d762ed701efaab722dd9db9bf113a36
Jorge Valencia <info@jorgevalencia.co>
2016-04-28 00:13:43 +0200

    animation y fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-        $( ".btn-anim-three" ).on( "click", function() {
-          $(this).toggleClass( "closed" );
-          $('.menu-mobile').toggleClass( "closed" );
-        });
 
-        
-  		});
+    //Animation mobile menu
+    $( ".btn-anim-three" ).on( "click", function() {
+      $(this).toggleClass( "closed" );
+      $('.menu-mobile').toggleClass( "closed" );
+    });
+
+    //Animation content
+    $('.js-animate').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown',
+        offset: 100
+       });
+
+});

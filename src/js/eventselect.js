03e5648030b671d2af64eb296301210de943d943
itsaso <itsaso.altuna@gmail.com>
2016-05-24 18:51:42 +0200

    JS select event fixed

--- src/js/eventselect.js
+++ src/js/eventselect.js
@@ -1,33 +1,53 @@
 $(document).ready(function() {
-	$('.smartphone').show();
-	$('.tablet').show();
-	$('.portatil').show();
-	$('.desktop').show();
 
 	$('select').change(function(ev){
-		$('.smartphone').hide();
-		$('.tablet').hide();
-		$('.portatil').hide();
-		$('.desktop').hide();
+		$('.evento_ficha').hide();
 
-		if ($("select option:selected").val() == 1) {
+		if ($("#dispositivo option:selected").val() == 1) {
 			$('.smartphone').show();
+
+			if ($("#lugar option:selected").val() == 6) {
+				$('.ateneu').hide();
+			} else if ($("#lugar option:selected").val() == 7) {
+				$('.bau').hide();
+			};
 
 		} else if ($("select option:selected").val() == 2) {
 			$('.tablet').show();
 
+			if ($("#lugar option:selected").val() == 1) {
+				$('.ateneu').hide();
+			} else if ($("#lugar option:selected").val() == 2) {
+				$('.bau').hide();
+			};
+
 		} else if ($("select option:selected").val() == 3) {
 			$('.portatil').show();
 
+			if ($("#lugar option:selected").val() == 1) {
+				$('.ateneu').hide();
+			} else if ($("#lugar option:selected").val() == 2) {
+				$('.bau').hide();
+			};
+
 		} else if ($("select option:selected").val() == 4) {
 			$('.desktop').show();
+
+			if ($("#lugar option:selected").val() == 1) {
+				$('.ateneu').hide();
+			} else if ($("#lugar option:selected").val() == 2) {
+				$('.bau').hide();
+			};
 		
 		} else {
-			$('.smartphone').show();
-			$('.tablet').show();
-			$('.portatil').show();
-			$('.desktop').show();
-		} 
+			$('.evento_ficha').show();
+
+			if ($("#lugar option:selected").val() == 1) {
+				$('.ateneu').hide();
+			} else if ($("#lugar option:selected").val() == 2) {
+				$('.bau').hide();
+			};
+		}
 	})		
 
 });

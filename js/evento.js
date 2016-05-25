2945663e679c27bc6346a393015d5d1eeebb5a47
itsaso <itsaso.altuna@gmail.com>
2016-05-25 03:39:13 +0200

    Update 2016-05-25T01:39:11.687Z

--- js/evento.js
+++ js/evento.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
-  $('.taller_detail-box').click(function(ev) {
-    $(this).toggleClass("open");
-  });
+  
 
 });
 
--- js/scroll2.js
+++
@@ -1,4 +0,0 @@
-$('#to-donaciones').click(function(ev) {
-	ev.preventDefault();
-	$(window).scrollTo('#donaciones',700);
-});

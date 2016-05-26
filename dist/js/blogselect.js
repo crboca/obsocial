abca82a959099b172a29fbe04b0fc1ee2b13d42e
itsaso <itsaso.altuna@gmail.com>
2016-05-27 01:21:25 +0200

    blog grid bug fixed

--- dist/js/blogselect.js
+++ dist/js/blogselect.js
@@ -9,21 +9,45 @@ $(document).ready(function() {
 			$('.eventos').show();
 			$('.donaciones').hide();
 			$('.noticias').hide();
+			
+			$('.grid').masonry({
+  				itemSelector: '.grid_item',
+  				columnWidth: '.grid_sizer',
+  				percentPosition: true
+			});
 
 		} else if ($("select option:selected").val() == 2) {
 			$('.donaciones').show();
 			$('.eventos').hide();
 			$('.noticias').hide();
 
+			$('.grid').masonry({
+  				itemSelector: '.grid_item',
+  				columnWidth: '.grid_sizer',
+  				percentPosition: true
+			});
+
 		} else if ($("select option:selected").val() == 3) {
 			$('.noticias').show();
 			$('.eventos').hide();
 			$('.donaciones').hide();
+
+			$('.grid').masonry({
+  				itemSelector: '.grid_item',
+  				columnWidth: '.grid_sizer',
+  				percentPosition: true
+			});
 		
 		} else {
 			$('.eventos').show();
 			$('.donaciones').show();
 			$('.noticias').show();
+
+			$('.grid').masonry({
+  				itemSelector: '.grid_item',
+  				columnWidth: '.grid_sizer',
+  				percentPosition: true
+			});
 		} 
 	})		
 

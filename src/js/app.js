1eed6aba474bae7e1929872aab061911ed0cef2a
itsaso <itsaso.altuna@gmail.com>
2016-05-27 03:13:29 +0200

    btn subir automatico WIP

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,22 @@ $(document).ready(function() {
       $(this).toggleClass("open");
     });
 
+    //scroll general subir pagina
+    $(".btn_subir").hide();
+
+    $('#to-header').click(function(ev) {
+      ev.preventDefault();
+      $(window).scrollTo('#header',700);
+    });
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() < $(window).height()) {
+          $(".btn_subir").fadeOut(400);
+      } else {
+          $(".btn_subir").fadeIn(400);
+      };
+    })
+
     //scroll especial index
     $('#to-donaciones').click(function(ev) {
       ev.preventDefault();

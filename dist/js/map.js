c8a95a58ef11ec711d7d8d0ee428d8cdfd834a8d
itsaso <itsaso.altuna@gmail.com>
2016-05-31 11:45:07 +0200

    mapa WIP

---
+++ dist/js/map.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  $('#index_map--country').select(function(ev) {
+    $('.index_map--message').html("En "+pais+" se generan "+kilos+"kg de basura electrónica por habitante al año." )
+  });
+    
+});

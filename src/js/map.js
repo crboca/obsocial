3b6fb8438bcb689b54463b53bff61d88f3837a5c
itsaso <itsaso.altuna@gmail.com>
2016-05-31 13:54:26 +0200

    mensaje que cambia mapa

--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 
-  $('#index_map--country').select(function(ev) {
-    $('.index_map--message').html("En "+pais+" se generan "+kilos+"kg de basura electrónica por habitante al año." )
+  $("#country").change(function(ev) {
+    // var frase = "En "+ country.pais +" se generan "+ country.kilo +"kg de basura electrónica por habitante al año.";
+
+    if ($("#country").val() == "espana") {
+      var country ={ pais: 'España',
+          kilo: 17.77}
+    } else if ($("#country").val() == "alemania") {
+      var country ={ pais: 'Alemania',
+          kilo: 21.68}
+    } else if ($("#country").val() == "francia") {
+      var country ={ pais: 'Francia',
+          kilo: 22.17}
+    } else if ($("#country").val() == "portugal") {
+      var country ={ pais: 'Portugal',
+          kilo: 16.17}
+    } else if ($("#country").val() == "reinounido") {
+      var country ={ pais: 'Reino Unido',
+          kilo: 23.51}
+    } else {
+      var country ={ pais: 'Irlanda',
+          kilo: 19.81}
+    };
+
+    $('.index_map--message').html("En "+ country.pais +" se generan "+ country.kilo +"kg de basura electrónica por habitante al año.");
   });
     
 });
